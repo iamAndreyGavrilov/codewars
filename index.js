9eeb9484664698ac9a2161acb7acc4c5c68604b1
Andrey Gavrilov <a.gavrilov@promo-bot.ru>
2022-07-25 17:48:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // ==========
+// function countPositivesSumNegatives(input) {
+//   if (input === null || input.length < 1) {
+//     return [];
+//   }
+//   let array = [0, 0];
 
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i] <= 0) {
+//       array[1] += input[i];
+//     } else {
+//       array[0] += 1;
+//     }
+//   }
+//   return array;
+// }
 
+// console.log(
+//   countPositivesSumNegatives([
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+//   ])
+// );
 // ==========
 // function fakeBin(x) {
 //   let a = x.split("");
