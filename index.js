6939cc76d73512b007b8f625985873fca2e5213a
Andrey Gavrilov <a.gavrilov@promo-bot.ru>
2022-07-28 19:33:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // ==========
+
+// var uniqueInOrder = function (iterable) {
+//   let arr = [];
+//   for (let i = 0; i < iterable.length; i++) {
+//     if (iterable[i] !== iterable[i + 1]) {
+//       arr.push(iterable[i]);
+//     }
+//   }
+//   return arr;
+// };
+
+// console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+
+// ==========
 // function isIsogram(str) {
 //   let strLowerCase = str.toLowerCase();
 //   let strSplit = strLowerCase.split("");
